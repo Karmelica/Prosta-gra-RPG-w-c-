e3cba57d0c3d58683cc658f19ec56b7204c24219
Damian Niedziolka <damianniedziolka7@gmail.com>
2022-11-29 23:05:44 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -9,7 +9,7 @@ int r(int p, int k)
     return n;
 }
 
-float atak(int atk, int chance, int multi)
+float atak(float atk, float chance, float multi)
 {
     int szansa = r(1,100);
     if(chance>=szansa)
@@ -22,21 +22,67 @@ float atak(int atk, int chance, int multi)
     }
 }
 
-vector <float> staty()
+//vector <float> stat(int lvl)
+//{
+//    int hp, atk, cR;
+//    float cDMG;
+//    vector <float> S;
+//    hp = r(300, 200);
+//    atk = r(25, 15);
+//    cR = r(1, 40);
+//    cDMG = r(101, 49);
+//    S.push_back(hp+(0.5*lvl));
+//    S.push_back(atk+(0.5*lvl));
+//    S.push_back(cR+(0.3*lvl));
+//    S.push_back(cDMG/100+(0.6*lvl));
+//    return S;
+//}
+
+int walka(struct postac P, struct postac W)
 {
-    int hp, atk, cR;
-    float cDMG;
-    vector <float> STATY;
-    hp = r(3000, 2000);
-    atk = r(250, 150);
-    cR = r(1, 40);
-    cDMG = r(101, 49);
-    STATY.push_back(hp);
-    STATY.push_back(atk);
-    STATY.push_back(cR);
-    STATY.push_back(cDMG/100);
-    return STATY;
+    cout<<"     Walka!      \n";
+    do
+    {
+        cout<<"\nHP twoje: "<<P.hp<<", HP Wroga: "<<W.hp<<endl;
+        cout<<"Uderzasz za "<<atak(P.atk,P.cR,P.cDMG)<<" ";
+        W.hp-=atak(P.atk,P.cR,P.cDMG);
+        if(W.hp<0)
+        {
+            return 1;
+            break;
+        }
+        cout<<"Wrog uderza za "<<atak(W.atk,W.cR,W.cDMG)<<endl;
+        P.hp-=atak(W.atk,W.cR,W.cDMG);
+        if(P.hp<0)
+        {
+            return 0;
+            break;
+        }
+    }
+    while(P[0]>0);
+
+
+
+    cout<<endl<<"\n Kliknij enter aby kontynuowac";
+    getchar();
 }
+
+struct postac
+{
+    float hp;
+    float atk;
+    float cR;
+    float cDMG;
+};
+
+void pokaz(struct postac A)
+{
+    cout<<A.hp<<" ";
+    cout<<A.atk<<" ";
+    cout<<A.cR<<" ";
+    cout<<A.cDMG<<" ";
+}
+
 
 int main()
 {
@@ -46,57 +92,53 @@ int main()
     vector <float> P;
     vector <float> W;
 
-
-
-    cout<<"Kliknij enter aby kontynuowac";
+    cout<<"\n Kliknij enter aby kontynuowac";
     getchar();
 
-
-
-    cout<<"\n Postac | HP | ATK | CritRate | CritDMG |\n";
-    cout<<" Twoja Postac: ";
-    for (int i=0; i<staty().size(); i++)
+    int lvl = 200;
+    postac Gracz =
     {
-        P.push_back(staty()[i]);
-        cout<<P[i]<<" ";
-    }
+        r(300, 200)+(0.5*lvl),
+        r(25, 15)+(0.5*lvl),
+        r(1, 40)+(0.3*lvl),
+        (r(101, 49)+(0.6*lvl))/100,
+    };
+
+    postac Wrog =
+    {
+        r(300, 200)+(0.5*lvl),
+        r(25, 15)+(0.5*lvl),
+        r(1, 40)+(0.3*lvl),
+        (r(101, 49)+(0.6*lvl))/100,
+    };
+
+    cout<<"\n Postac | HP | ATK | CritRate | CritDMG |";
+    cout<<"\n Twoja Postac: ";
+    pokaz(Gracz);
     cout<<"\n Wrog: ";
-    for (int i=0; i<staty().size(); i++)
-    {
-        W.push_back(staty()[i]);
-        cout<<W[i]<<" ";
-    }
+    pokaz(Wrog);
+//    for (int i=0; i<stat(lvl).size(); i++)
+//    {
+//        P.push_back(stat(lvl)[i]);
+//        cout<<P[i]<<" | ";
+//    }
+//    for (int i=0; i<stat(lvl).size(); i++)
+//    {
+//        W.push_back(stat(lvl)[i]);
+//        cout<<W[i]<<" | ";
+//    }
 
-
-
-    cout<<endl<<"\nKliknij enter aby kontynuowac";
+    cout<<"\n Kliknij enter aby kontynuowac";
     getchar();
 
-
-
-    cout<<"     Walka!      \n";
-    do
+    if(walka(Gracz, Wrog)==1)
     {
-        cout<<"\nHP twoje: "<<P[0]<<", HP Wroga: "<<W[0]<<endl;
-        cout<<"Uderzasz za "<<atak(P[1],P[2],P[3])<<" ";
-        W[0]-=atak(P[1],P[2],P[3]);
-        if(W[0]<0)
-        {
-            cout<<"\n Wygrales ";
-            break;
-        }
-        cout<<"Wrog uderza za "<<atak(W[1],W[2],W[3])<<endl;
-        P[0]-=atak(W[1],W[2],W[3]);
-        if(P[0]<0)
-        {
-            cout<<"\n Przegrales ";
-            break;
-        }
+        cout<<"\n Wygrales";
     }
-    while(P[0]>0);
-
-
-
+    else
+    {
+        cout<<"\n Przegrales";
+    }
 //    wy.close();
     return 0;
 }
