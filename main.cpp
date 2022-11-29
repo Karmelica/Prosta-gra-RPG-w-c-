6aaf5c2686bebf5b87b7c0651a4b05c398f43c56
Damian Niedziolka <damianniedziolka7@gmail.com>
2022-11-29 18:08:06 +0100

    serio

--- main.cpp
+++ main.cpp
@@ -7,35 +7,65 @@ float cDMG;
 int Whp, Watk, WcR;
 float WcDMG;
 
-int r(int p, int k){
+int r(int p, int k)
+{
     int n;
     n = p + rand() % k;
     return n;
 }
 
-void postac(){
-    hp = r(200, 800); atk = r(50, 100); cR = r(1, 40); cDMG = r(101, 49);
-    cout<<" \nTwoja postac:\nhp:"<<hp<<" \natk: "<<atk<<" \ncR: "<<cR<<" \ncDMG: "<<cDMG/100<<endl;
+vector <float> postac()
+{
+    vector <float> POSTAC;
+    hp = r(200, 800);
+    atk = r(50, 100);
+    cR = r(1, 40);
+    cDMG = r(101, 49);
+    POSTAC.push_back(hp);
+    POSTAC.push_back(atk);
+    POSTAC.push_back(cR);
+    POSTAC.push_back(cDMG/100);
+    return POSTAC;
 }
 
-void wrog(){
-    Whp = r(200, 800); Watk = r(50, 100); WcR = r(1, 40); WcDMG = r(101, 49);
-    cout<<" \nWroga postac:\nhp:"<<Whp<<" \natk: "<<Watk<<" \ncR: "<<WcR<<" \ncDMG: "<<WcDMG/100<<endl;
+vector <float> wrog()
+{
+    vector <float> WROG;
+    Whp = r(200, 800);
+    Watk = r(50, 100);
+    WcR = r(1, 40);
+    WcDMG = r(101, 49);
+    WROG.push_back(Whp);
+    WROG.push_back(Watk);
+    WROG.push_back(WcR);
+    WROG.push_back(WcDMG/100);
+    return WROG;
 }
-
 
 int main()
 {
     srand(time(NULL));
     cout<<" Witaj, wybierz opcje:\n1. Graj\n2. Wyjdz\n";
     int wybor;
-    do{
+    do
+    {
         cin>>wybor;
-    }while(wybor!=1 && wybor!=2);
-    switch(wybor){
+    }
+    while(wybor!=1 && wybor!=2);
+    switch(wybor)
+    {
     case 1:
-        postac();
-        wrog();
+        cout<<" Postac | HP | ATK | CritRate | CritDMG |\n";
+        cout<<" Twoja postac: ";
+        for (int i=0; i<postac().size(); i++)
+        {
+            cout<<postac()[i]<<" ";
+        }
+        cout<<"\n Wrog: ";
+        for (int i=0; i<wrog().size(); i++)
+        {
+            cout<<wrog()[i]<<" ";
+        }
         break;
     case 2:
         cout<<"Pa Pa";
