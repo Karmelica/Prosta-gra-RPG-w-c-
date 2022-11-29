7efba39ea29b77cadf176a5e175b2892477ef69c
Damian Niedziolka <damianniedziolka7@gmail.com>
2022-11-29 19:29:01 +0100

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -2,11 +2,6 @@
 
 using namespace std;
 
-int hp, atk, cR;
-float cDMG;
-int Whp, Watk, WcR;
-float WcDMG;
-
 int r(int p, int k)
 {
     int n;
@@ -14,33 +9,41 @@ int r(int p, int k)
     return n;
 }
 
-vector <float> postac()
-{
-    vector <float> POSTAC;
-    hp = r(200, 800);
-    atk = r(50, 100);
-    cR = r(1, 40);
-    cDMG = r(101, 49);
-    POSTAC.push_back(hp);
-    POSTAC.push_back(atk);
-    POSTAC.push_back(cR);
-    POSTAC.push_back(cDMG/100);
-    return POSTAC;
+bool los(int chance){
+    int szansa = r(1,chance);
+    if(){};
+
 }
 
-vector <float> wrog()
+vector <float> staty()
 {
-    vector <float> WROG;
-    Whp = r(200, 800);
-    Watk = r(50, 100);
-    WcR = r(1, 40);
-    WcDMG = r(101, 49);
-    WROG.push_back(Whp);
-    WROG.push_back(Watk);
-    WROG.push_back(WcR);
-    WROG.push_back(WcDMG/100);
-    return WROG;
+    int hp, atk, cR;
+    float cDMG;
+    vector <float> STATY;
+    hp = r(300, 200);
+    atk = r(25, 15);
+    cR = r(1, 40);
+    cDMG = r(101, 49);
+    STATY.push_back(hp);
+    STATY.push_back(atk);
+    STATY.push_back(cR);
+    STATY.push_back(cDMG/100);
+    return STATY;
 }
+
+//vector <float> wrog()
+//{
+//    vector <float> WROG;
+//    Whp = r(200, 800);
+//    Watk = r(50, 100);
+//    WcR = r(1, 40);
+//    WcDMG = r(101, 49);
+//    WROG.push_back(Whp);
+//    WROG.push_back(Watk);
+//    WROG.push_back(WcR);
+//    WROG.push_back(WcDMG/100);
+//    return WROG;
+//}
 
 int main()
 {
@@ -49,40 +52,60 @@ int main()
     srand(time(NULL));
     vector <float> POSTAC;
     vector <float> WROG;
+
+
+
     cout<<"Kliknij enter aby kontynuowac";
     getchar();
+
+
+
+
     cout<<"\n Postac | HP | ATK | CritRate | CritDMG |\n";
     cout<<" Twoja postac: ";
-    for (int i=0; i<postac().size(); i++)
+    for (int i=0; i<staty().size(); i++)
     {
-        POSTAC.push_back(postac()[i]);
+        POSTAC.push_back(staty()[i]);
         cout<<POSTAC[i]<<" ";
     }
     cout<<"\n Wrog: ";
-    for (int i=0; i<wrog().size(); i++)
+    for (int i=0; i<staty().size(); i++)
     {
-        WROG.push_back(wrog()[i]);
+        WROG.push_back(staty()[i]);
         cout<<WROG[i]<<" ";
     }
+
+
+
     cout<<endl<<"\nKliknij enter aby kontynuowac";
     getchar();
 
-    cout<<"     Walka!\n";
+
+
+    cout<<"     Walka!      \n";
     do{
         cout<<"HP twoje: "<<POSTAC[0]<<" HP wroga: "<<WROG[0]<<endl;
-        WROG[0]-=POSTAC[1];
+        WROG[0]-=POSTAC[1]*POSTAC[3];
+        cout<<"Uderzasz za "<<POSTAC[1]*POSTAC[3]<<" ";
         if(WROG[0]<0){
             cout<<"\n Wygrales ";
             break;
         }
-        POSTAC[0]-=WROG[1];
+        POSTAC[0]-=WROG[1]*WROG[3];
+        cout<<"Wrog uderza za "<<WROG[1]*WROG[3]<<endl;
         if(POSTAC[0]<0){
             cout<<"\n Przegrales ";
             break;
         }
     }while(POSTAC[0]>0);
+
+
+
     cout<<endl<<"\nKliknij enter aby kontynuowac";
     getchar();
+
+
+
 //    wy.close();
     return 0;
 }
